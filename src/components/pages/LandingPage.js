d5c406879997f77560ff96fb44fa11ff03eda823
john feyisayo <johnfeyisayo@gmail.com>
2023-03-25 18:56:13 +0100

    update files

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -2,60 +2,176 @@ import React from 'react';
 import Logo from './images/logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Portrait from './images/portrait-young-african-woman-with-laptop-white.png';
+import Monitor from './images/Monitor.jpg';
+import People from './images/People.jpg';
+import People2 from './images/People2.jpg';
+import Line from './images/Line.png';
+import Carousel from 'react-bootstrap/Carousel';
+import Ui from './images/UiUxImage.png';
+import Frontend from './images/Frontend.png';
+import Backend from './images/Backend.png';
+// import DataScience from './images/DataScience.png';
+// import MobileDev from './images/MobileDev.png';
+// import SotwareTesting from './images/SoftwareTesting.png';
 // import Citrone from './images/citrone.png';
+import Student from './images/Student & Tutors.png';
+import Questions from './images/QUIZ questions 1.png';
+import LessonPage from './images/Lesson Page.png';
+import Dashboard from './images/Dashboard.png';
+import classes from './LandingPage.module.css';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
 
 const LandingPage = () => {
   return (
-    <>
-      <section className="first-container">
-        <div className="header-container">
-          <img className="logo" src={Logo} alt="" />
-          <h2 className="citrone">Citrone</h2>
+    <div className={classes.landingPage}>
+      <section className={classes['first-container']}>
+        <div className={classes['header-container']}>
+          <img className={classes.logo} src={Logo} alt="" />
+          <h2 className={classes.citrone}>Citrone</h2>
           <Link to="/login">
-            <Button variant="secondary" className="login-button mb-10">
-              Log In
-            </Button>
+            <button className={classes['login-button']}>Log In</button>
           </Link>
         </div>
-        <div className="section-landing-1">
-          <div className="paragraph-container">
-            <h1 className="first-paragraph">
-              Delivering a <span className="community">community-first </span>
+        <div className={classes['section-landing-1']}>
+          <div className={classes['paragraph-container']}>
+            <h1 className={classes['first-paragraph']}>
+              Delivering a <span className={classes.community}>community-first </span>
               learning experience
             </h1>
-            <p className="second-paragraph">
-              Citrone connects <span className="certified">certified mentors</span> to all
+            <p className={classes['second-paragraph']}>
+              Citrone connects <span className={classes.certified}>certified mentors</span> to all
               interested in a tech career. It guarantees a learning experience that transcends a
               real world classroom.
             </p>
             <Link to="/signup">
-              <button className="start-career-button">Start Tech Career</button>
+              <button className={classes['start-career-button']}>Start Tech Career</button>
             </Link>
           </div>
           <div>
-            <img className="portrait" src={Portrait} alt="" />
+            <img className={classes.portrait} src={Portrait} alt="" />
           </div>
         </div>
       </section>
-      <div className="learning-process-container">
-        <div className="learning-process-text-container">
-          <div className="learning-process-text1">
-            <p>Learn online </p>
-            <p>at a flexible pace</p>
+      <div className={classes['learning-process-container']}>
+        <div className={classes['learning-process-text-container']}>
+          <div>
+            <img src={Monitor} alt="" />
+          </div>
+          <div className={classes['learning-process-text1']}>
+            <p id={classes['learning-process-text']}>Learn online </p>
+            <p id={classes['learning-process-text2']}>at a flexible pace</p>
           </div>
           <div>
-            <p>Engage industry certified mentors </p>
-            <p>to share ideas to learn faster </p>
+            <img src={Line} alt="" />
           </div>
           <div>
-            <p>Collaborate with peers</p>
-            <p>to share ideas to learn faster</p>
+            <img src={People} alt="" />
+          </div>
+          <div>
+            <p id={classes['learning-process-text']}>Engage industry certified mentors </p>
+            <p id={classes['learning-process-text2']}>to share ideas to learn faster </p>
+          </div>
+          <div>
+            <img src={Line} alt="" />
+          </div>
+          <div>
+            <img src={People2} alt="" />
+          </div>
+          <div>
+            <p id={classes['learning-process-text']}>Collaborate with peers</p>
+            <p id={classes['learning-process-text2']}>to share ideas to learn faster</p>
           </div>
         </div>
       </div>
-    </>
+      <section className={classes['second-container']}>
+        <h2 id={classes['second-container-heading']}>Our course selection</h2>
+        <>
+          <Carousel slide={false}>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                id={classes['carousel-image']}
+                src={Ui}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                id={classes['carousel-image']}
+                src={Frontend}
+                alt="Second slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                id={classes['carousel-image']}
+                src={Backend}
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </>
+        <div>
+          <h2 id={classes['second-container-heading']}>Community-first learning</h2>
+          <div className={classes['second-container-paragraph']}>
+            <p>
+              Encouraging a community-first learning experience can be highly beneficial for
+              students
+            </p>
+            <p>as it creates a sense of connection and support that can enhance</p>
+            <p>their overall educational experience.</p>
+          </div>
+        </div>
+        <div className={classes['second-container-image-container']}>
+          <div>
+            <img src={Student} alt="" />
+          </div>
+          <div>
+            <img src={Questions} alt="" />
+          </div>
+        </div>
+      </section>
+      <section className={classes['Third-section-container']}>
+        <h1 className={classes['third-section-heading']}>Effective learning</h1>
+        <div className={classes['second-container-paragraph']}>
+          <p>Engaging in training sessions designed by certified mentors is a great way</p>
+          <p>
+            to ensure that you are receiving high-quality instruction from experienced professionals
+          </p>
+          <p>who are skilled at teaching and facilitating learning.</p>
+          <p id={classes.interactive}>
+            Interactive Videos. Informative slides. Self-assessment. Much more.
+          </p>
+        </div>
+        <div className={classes['third-section-image']}>
+          <div>
+            <img src={LessonPage} alt="" />
+          </div>
+          <div>
+            <img src={Dashboard} alt="" />
+          </div>
+        </div>
+        {/* <footer>
+
+        </footer> */}
+      </section>
+    </div>
   );
 };
 
