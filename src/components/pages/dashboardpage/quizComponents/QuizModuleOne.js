1e2f1e00916bfb8b57b102821f2bab3841832515
john feyisayo <johnfeyisayo@gmail.com>
2023-04-26 00:44:21 +0100

    update quiz module one

--- src/components/pages/dashboardpage/quizComponents/QuizModuleOne.js
+++ src/components/pages/dashboardpage/quizComponents/QuizModuleOne.js
@@ -4,6 +4,7 @@ import styles from '../db_ components/Dashboard.module.css';
 import SideNavBar from '../db_ components/SideNavBar';
 import classes from './Quiz.module.css';
 import Line from '../images/Line.png';
+import { Link } from 'react-router-dom';
 
 const ModuleOne = () => {
   return (
@@ -18,9 +19,11 @@ const ModuleOne = () => {
             </div>
           </div>
           <div className={classes['quiz-moduleone-first-paragraph']}>
-            <div>
-              <p id={classes['quiz-moduleone-first-paragraph1']}>Quizzes</p>
-            </div>
+            <Link to="/quizzes" style={{ textDecoration: 'none' }}>
+              <div>
+                <p id={classes['quiz-moduleone-first-paragraph1']}>Quizzes</p>
+              </div>
+            </Link>
             <div className={classes['quiz-moduleone-first-paragraph2']}>
               <p>&gt;</p>
             </div>
@@ -30,15 +33,15 @@ const ModuleOne = () => {
           </div>
           <h4 id={classes['quiz-moduleone-first-paragraph4']}>Module 1 Quizzes</h4>
           <div className={classes['quiz1-card']}>
-            <div>
+            <div className={classes.quiz1}>
               <h5>Quiz 1</h5>
             </div>
             <div>
-              <img src={Line} alt="" />
+              <img className={classes['quiz1-line']} src={Line} alt="" />
             </div>
-            <div>
-              <h4>Introduction to product design</h4>
-              <div className={classes['quiz1-card-sub-container']}>
+            <div className={classes['quiz1-second-container']}>
+              <p id={classes.introduction}>Introduction to product design</p>
+              <div className={classes['quiz1-card-second-sub-container']}>
                 <div>
                   <p>Ques: 10</p>
                 </div>
@@ -53,8 +56,92 @@ const ModuleOne = () => {
                 </div>
               </div>
             </div>
-            <div>
+            <div className={classes.graded}>
               <p>Graded</p>
+            </div>
+          </div>
+          <div className={classes['quiz1-card2-5']}>
+            <div className={classes.quiz1}>
+              <h5>Quiz 2</h5>
+            </div>
+            <div>
+              <img className={classes['quiz1-line']} src={Line} alt="" />
+            </div>
+            <div className={classes['quiz1-second-container']}>
+              <p id={classes.introduction}>Introduction to product design</p>
+              <div className={classes['quiz1-card-second-sub-container']}>
+                <div>
+                  <p>Ques: 10</p>
+                </div>
+                <div>
+                  <p>Time: 10 minutes </p>
+                </div>
+                <div>
+                  <p>Attempts: 1</p>
+                </div>
+                <div>
+                  <p>Score: 0</p>
+                </div>
+              </div>
+            </div>
+            <div className={classes.start}>
+              <p>Start</p>
+            </div>
+          </div>
+          <div className={classes['quiz1-card']}>
+            <div className={classes.quiz1}>
+              <h5>Quiz 3</h5>
+            </div>
+            <div>
+              <img className={classes['quiz1-line']} src={Line} alt="" />
+            </div>
+            <div className={classes['quiz1-second-container']}>
+              <p id={classes.introduction}>Introduction to product design</p>
+              <div className={classes['quiz1-card-second-sub-container']}>
+                <div>
+                  <p>Ques: 10</p>
+                </div>
+                <div>
+                  <p>Time: 10 minutes </p>
+                </div>
+                <div>
+                  <p>Attempts: 1</p>
+                </div>
+                <div>
+                  <p>Score: 0</p>
+                </div>
+              </div>
+            </div>
+            <div className={classes.start}>
+              <p>Start</p>
+            </div>
+          </div>
+          <div className={classes['quiz1-card']}>
+            <div className={classes.quiz1}>
+              <h5>Quiz 4</h5>
+            </div>
+            <div>
+              <img className={classes['quiz1-line']} src={Line} alt="" />
+            </div>
+            <div className={classes['quiz1-second-container']}>
+              <p id={classes.introduction}>Introduction to product design</p>
+              <div className={classes['quiz1-card-second-sub-container']}>
+                <div>
+                  <p>Ques: 10</p>
+                </div>
+                <div>
+                  <p>Time: 10 minutes </p>
+                </div>
+                <div>
+                  <p>Attempts: 1</p>
+                </div>
+                <div>
+                  <p>Score: 0</p>
+                </div>
+              </div>
+            </div>
+            <div className={classes.start}>
+              <p>Start</p>
             </div>
           </div>
         </div>
