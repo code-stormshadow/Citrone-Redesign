0662e30539df524916a909aa94872f6f3bf37faf
Emi-ife Faluyi <emiifefaluyi@gmail.com>
2023-04-26 23:39:54 +0100

    assignment update

--- src/components/pages/dashboardpage/Assignment Components/GradesBreakdown.js
+++ src/components/pages/dashboardpage/Assignment Components/GradesBreakdown.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from '../db_ components/NavBar';
 import SideNavBar from '../db_ components/SideNavBar';
 import styles from './Assignment.module.css';
+import Alarm from './Assignment Images/Alarm.svg';
 
 const GradesBreakdown = () => {
   return (
@@ -9,15 +10,18 @@ const GradesBreakdown = () => {
       <NavBar />
       <SideNavBar />
       <div className={styles.assignmentsMainWrapper}>
-        <div className={styles.gradesDiv}>
+        <div className={styles.gradesBreakdownDiv}>
           <h4 className={styles.assignmentHeaderOne}>
-            <span style={{ fontFamily: 'regular-mont' }}>Assignments</span> &#10095; Module 1
-            Assignments <span>&#10095; </span> Lesson 1 Grade &#10095;
+            Assignments &#10095; Module 1 Assignments &#10095;{' '}
+            <span className={styles.redHeader}>Lesson 1 Grade &#10095;</span>
             <span className={styles.redHeader}>Lesson 1 breakdown</span>
           </h4>
-          <div>
-            <h3 className={styles.gradesHeaderTwo}> Module 1; Lesson 1</h3>
-            <h3> Due: 19 Sep, 2022.</h3>
+          <div className={styles.gradesHeaderTwo}>
+            <h3> Module 1; Lesson 1</h3>
+            <h3 className={styles.gradesBreakdownDueDate}>
+              <img src={Alarm}></img>{' '}
+              <span className={styles.dueDateRightHeader}>Due: 19 Sep, 2022.</span>
+            </h3>
           </div>
         </div>
       </div>
