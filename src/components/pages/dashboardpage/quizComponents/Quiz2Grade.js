8452d2b3248eafe2c3d5bb6a19e543f8cef2b223
john feyisayo <johnfeyisayo@gmail.com>
2023-04-27 20:04:55 +0100

    collect latest updates

--- src/components/pages/dashboardpage/quizComponents/Quiz2Grade.js
+++ src/components/pages/dashboardpage/quizComponents/Quiz2Grade.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavBar from '../db_ components/NavBar';
 import styles from '../db_ components/Dashboard.module.css';
 import SideNavBar from '../db_ components/SideNavBar';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import classes from './Quiz.module.css';
 
 const Quiz2Grade = () => {
@@ -11,23 +11,8 @@ const Quiz2Grade = () => {
       <NavBar />
       <div className={styles.wrapper}>
         <SideNavBar />
-        <div>
-          {/* <div>
-            <input placeholder="Search module title or lesson" className={styles.searchBarQuiz} />
-          </div> */}
-          <div className={classes['quiz-moduleone-first-paragraph']}>
-            <Link to="/quizzes" style={{ textDecoration: 'none' }}>
-              <div>
-                <p id={classes['quiz-moduleone-first-paragraph1']}>Quizzes</p>
-              </div>
-            </Link>
-            <div className={classes['quiz-moduleone-first-paragraph2']}>
-              <p>&gt;</p>
-            </div>
-            <div>
-              <p id={classes['quiz-moduleone-first-paragraph3']}>Module 1 Quizzes</p>
-            </div>
-          </div>
+        <div className={classes['quiz2grade-container']}>
+          <h1>Hello</h1>
         </div>
       </div>
     </div>
