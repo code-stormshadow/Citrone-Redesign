e64efc2600865cccfaf7effecf0d72f0755c4024
Eboweme Ayo <ebowemeayo@yahoo.com>
2023-05-10 09:49:47 +0100

    get updated work

--- src/components/pages/dashboardpage/Settings.js
+++ src/components/pages/dashboardpage/Settings.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './db_ components/NavBar';
 import SideNavBar from './db_ components/SideNavBar';
 import styles from './db_ components/Dashboard.module.css';
+import BannerImage from './images/banner.png';
 
 const Settings = () => {
   return (
@@ -9,7 +10,9 @@ const Settings = () => {
       <NavBar />
       <div className={styles.wrapper}>
         <SideNavBar />
-        <div></div>
+        <div className={styles.settingsWrapper}>
+          <img src={BannerImage} alt="Banner Image" className={styles.bannerImage} />
+        </div>
       </div>
     </div>
   );
